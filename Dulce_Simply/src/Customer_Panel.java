f15ef7370d6750133708842540a8e8bea165ed37
Yonshisoru <44347209+Yonshisoru@users.noreply.github.com>
2019-01-17 02:44:27 +0700

    Link Menu & Promotion Table into SQL

--- Dulce_Simply/src/Customer_Panel.java
+++ Dulce_Simply/src/Customer_Panel.java
@@ -1,3 +1,11 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,14 +17,147 @@
  * @author Yonshisoru
  */
 public class Customer_Panel extends javax.swing.JFrame {
+//---------------------------------------------------
+    Database d = new Database();
+//-------------------------------------------------
+ArrayList<Menu_variable>Menu_Array = new ArrayList<>();
+ArrayList<Menu_variable>Menu_List_Array = new ArrayList<>();
+ArrayList<Menu_variable>Promotion_List_Array = new ArrayList<>();
+ArrayList<Promotion_variable>Promotion_Array = new ArrayList<>();
+//-----------------------------------------------------------
+Connection con = null;
+PreparedStatement pat = null;
+ResultSet rs = null;
 
     /**
      * Creates new form Customer_Panel
      */
     public Customer_Panel() {
         initComponents();
+        getMenu_Catagory();
+        getMenu();
+        showmenu_table();
+        getPromotion();
+        getPromotion_Menu();
+        showpromotion_table();
     }
-
+    public Connection getcon(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection(d.url(),d.username(),d.password());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("Didn';t connect");
+            throw new RuntimeException(e);
+        }
+    }
+    public void getMenu_Catagory(){
+        String sql = "Select M_T_ID,M_T_NAME FROM MENU_TYPE WHERE M_T_DEL = 'N'";
+        try{
+            pat = getcon().prepareStatement(sql);
+            rs = pat.executeQuery(sql);
+            while(rs.next()){
+                Menu_variable m = new Menu_variable();
+                m.setcataid(rs.getString("M_T_ID"));
+                m.setcataname(rs.getString("M_T_NAME"));
+                Menu_List_Array.add(m);
+            }
+            rs.close();
+            pat.close();
+            getcon().close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    public void getMenu(){
+        String sql = "Select MENU_ID,MENU_NAME,MENU_PRICE,M_T_ID FROM MENU WHERE MENU_DEL = 'N'";
+        try{
+            pat = getcon().prepareStatement(sql);
+            rs = pat.executeQuery(sql);
+            while(rs.next()){
+                Menu_variable m = new Menu_variable();
+                m.setid(rs.getString("MENU_ID"));
+                m.setname(rs.getString("MENU_NAME"));
+                m.setprice(rs.getInt("MENU_PRICE"));
+                m.setcataid(rs.getString("M_T_ID"));
+                for(Menu_variable mv:Menu_List_Array){
+                    if(mv.getcataid().equals(m.getcataid())){
+                        m.setcataname(mv.getcataname());
+                        break;
+                    }
+                }
+                Menu_Array.add(m);
+            }
+            rs.close();
+            pat.close();
+            getcon().close();
+        }catch(Exception e){
+            
+        }
+    }
+    public void getPromotion(){
+        String sql = "Select PN_ID,PN_NAME,PN_S_DATE,PN_E_DATE FROM PROMOTION WHERE PN_DEL = 'N'";
+        try{
+            pat = getcon().prepareStatement(sql);
+            rs = pat.executeQuery(sql);
+            while(rs.next()){
+                Promotion_variable p = new Promotion_variable();
+                p.setid(rs.getString("PN_ID"));
+                p.setname(rs.getString("PN_NAME"));
+                p.setstartdate(rs.getString("PN_S_DATE"));
+                p.setenddate(rs.getString("PN_E_DATE"));
+                Promotion_Array.add(p);
+            }
+            rs.close();
+            pat.close();
+            getcon().close();
+        }catch(Exception e){
+            
+        }
+    }
+    public void getPromotion_Menu(){
+        String sql = "Select PM_ID,PN_ID,MENU_ID,PM_DISCOUNT FROM PROMOTION_MENU WHERE PM_DEL = 'N'";
+        try{
+            pat = getcon().prepareStatement(sql);
+            rs = pat.executeQuery(sql);
+            while(rs.next()){
+                Menu_variable p = new Menu_variable();
+                p.p.setmenunumber(rs.getString("PM_ID"));
+                p.p.setid(rs.getString("PN_ID"));
+                p.setid(rs.getString("MENU_ID"));
+                int k = (int)(rs.getDouble("PM_DISCOUNT")*(double)100);
+                p.p.setdiscount(k);
+                Promotion_List_Array.add(p);
+            }
+            rs.close();
+            pat.close();
+            getcon().close();
+        }catch(Exception e){
+            
+        }
+    }
+    public void showmenu_table(){
+        DefaultTableModel model = (DefaultTableModel)menu_table.getModel();
+        Object[] row = new Object[4];
+        for(int i =0;i<Menu_Array.size();i++){
+            row[0] = Menu_Array.get(i).getid();
+            row[1] = Menu_Array.get(i).getname();
+            row[2] = Menu_Array.get(i).getprice();
+            row[3] = Menu_Array.get(i).getcataname();   
+            model.addRow(row);
+        }
+    }
+    public void showpromotion_table(){
+        DefaultTableModel model = (DefaultTableModel)promotion_table.getModel();
+        Object[] row = new Object[4];
+        for(int i =0;i<Promotion_Array.size();i++){
+            row[0] = Promotion_Array.get(i).getid();
+            row[1] = Promotion_Array.get(i).getname();
+            row[2] = Promotion_Array.get(i).getstartdate();
+            row[3] = Promotion_Array.get(i).getenddate();   
+            model.addRow(row);
+        }
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -29,13 +170,13 @@ public class Customer_Panel extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        promotion_table = new javax.swing.JTable();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTable2 = new javax.swing.JTable();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jScrollPane3 = new javax.swing.JScrollPane();
-        jTable3 = new javax.swing.JTable();
+        menu_table = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
@@ -58,7 +199,7 @@ public class Customer_Panel extends javax.swing.JFrame {
         jLabel2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 50, 70, -1));
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        promotion_table.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -66,9 +207,9 @@ public class Customer_Panel extends javax.swing.JFrame {
                 "รหัสโปรโมชั่น", "ชื่อโปรโมชั่น", "วันเริ่มต้น", "วันสิ้นสุด"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(promotion_table);
 
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 400, 420, 230));
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 420, 420, 230));
 
         jTable2.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -82,13 +223,15 @@ public class Customer_Panel extends javax.swing.JFrame {
 
         getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 40, 640, -1));
 
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel3.setText("จำนวนเมนูทั้งหมด:");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(660, 490, -1, -1));
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 490, -1, -1));
 
+        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel4.setText("ราคารวม:");
         getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 540, -1, -1));
 
-        jTable3.setModel(new javax.swing.table.DefaultTableModel(
+        menu_table.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -96,7 +239,7 @@ public class Customer_Panel extends javax.swing.JFrame {
                 "รหัสเมนู", "ชื่อเมนู", "ราคา", "หมวดหมู่"
             }
         ));
-        jScrollPane3.setViewportView(jTable3);
+        jScrollPane3.setViewportView(menu_table);
 
         getContentPane().add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 140, 420, 230));
 
@@ -109,20 +252,25 @@ public class Customer_Panel extends javax.swing.JFrame {
         jButton3.setText("ปิด");
         getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(920, 610, -1, -1));
 
+        jLabel5.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel5.setText("ตารางเมนู");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 110, -1, -1));
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 100, -1, -1));
 
+        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel6.setText("ตารางโปรโมชั่น");
-        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 380, -1, -1));
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 380, -1, -1));
 
+        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel7.setText("ตารางออเดอร์");
         getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(810, 10, -1, 20));
 
+        jLabel8.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel8.setText("0");
-        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 540, -1, -1));
+        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 540, -1, -1));
 
+        jLabel9.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel9.setText("0");
-        getContentPane().add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 490, -1, -1));
+        getContentPane().add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 490, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -178,8 +326,8 @@ public class Customer_Panel extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
-    private javax.swing.JTable jTable1;
     private javax.swing.JTable jTable2;
-    private javax.swing.JTable jTable3;
+    private javax.swing.JTable menu_table;
+    private javax.swing.JTable promotion_table;
     // End of variables declaration//GEN-END:variables
 }
